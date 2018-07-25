40e097c01a3f57f936191388210959525c100105
takuya uchida <takuya.uchida@rakuten.com>
2018-07-25 05:18:06 +0000

    heroku v3

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -18,50 +18,69 @@ class FavoritesController extends Controller
     
     public function favorite(Request $request)
     {
-    	$shopfavorite = $request->input('shopfavorite');
-    // 	var_dump($shopfavorite);
-    	
-    	// $int = intval($shopfavorite[0]);
-    	// var_dump($int);
-    	
-    	
-    // 	$ints = [];
-    	$items = [];
-    	
-    	$count = count($shopfavorite);
-    	
-    	for ($i = 0; $i < $count; $i ++) {
-    // 		$int = intval($shopfavorite[$i]);
-            $id = $shopfavorite[$i];
-    		$query = Item::select('*')->join('shops', 'items.shop_id', '=', 'shops.id');
-    		$item = $query->where('items.image_path', '=', $id)->first();
-    		$items[$i] = $item;
-    	}
-    // 	var_dump($items);
-    	
+        $shopfavorite = $request->input('shopfavorite');
+        $items = [];
+        if(!empty($shopfavorite)) {
+        	
+            // var_dump($shopfavorite);
+        	
+        	// $int = intval($shopfavorite[0]);
+        	// var_dump($int);
+        	
+        	
+        // 	$ints = [];
+        	
+        	
+        	$count = count($shopfavorite);
+        	
+        	for ($i = 0; $i < $count; $i ++) {
+        // 		$int = intval($shopfavorite[$i]);
+                $id = $shopfavorite[$i];
+        		$query = Item::select('*')->join('shops', 'items.shop_id', '=', 'shops.id');
+        		$item = $query->where('items.image_path', '=', $id)->first();
+        		$items[$i] = $item;
+        	}
+        }
+        
     	$foodfavorite = $request->input('foodfavorite');
-    	
-    	$ints_food = [];
     	$restaurants = [];
+    	if(!empty($foodfavorite)) {
     	
-    	$count_food = count($foodfavorite);
-    	
-    	for ($j = 0; $j < $count_food; $j ++) {
-    		$int_food = intval($foodfavorite[$j]);
-    		$query = Restaurant::select('*');
-    		$restaurant = $query->where('restaurants.id', '=', $int_food)->first();
-    		$restaurants[$j] = $restaurant;
+        	$ints_food = [];
+        	
+        	
+        	$count_food = count($foodfavorite);
+        	
+        	for ($j = 0; $j < $count_food; $j ++) {
+        		$int_food = intval($foodfavorite[$j]);
+        		$query = Restaurant::select('*');
+        		$restaurant = $query->where('restaurants.id', '=', $int_food)->first();
+        		$restaurants[$j] = $restaurant;
+        	}
     	}
     	
     	$menus = Restaurant::select()->join('menus', 'restaurants.id', '=', 'menus.restaurant_id')->get();
     	$shouhin = Shop::select()->join('items', 'shops.id', '=', 'items.shop_id')->get();
     	
-    	return view('users.show', [
-    		'items' => $items,
-    		'restaurants' => $restaurants,
-    		'shouhin' => $items,
-    		'menus' => $menus,
-    		]);
+    	if($items != null && $restaurants != null) { 
+        	return view('users.show', [
+        		'items' => $items,
+        		'restaurants' => $restaurants,
+        		'shouhin' => $items,
+        		'menus' => $menus,
+        		]);
+    	} elseif ($items != null && $restaurants == null) {
+    	    return view('users.show', [
+        		'items' => $items,
+        		'shouhin' => $items,
+        		]);
+    	} elseif ($items == null && $restaurants != null) {
+    	    return view('users.show', [
+        		'restaurants' => $restaurants,
+        		'menus' => $menus,
+        		]);
+    	}
+    	
     	
     }
 }
